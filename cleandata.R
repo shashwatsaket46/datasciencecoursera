247839f5311c878c9c70bccdf5aa2b5f3d7e2da7
Shashwat Saket <52482910+shashwatsaket46@users.noreply.github.com>
2020-07-21 19:01:54 +0530

    run_Analysis.R

--- cleandata.R
+++ cleandata.R
@@ -44,4 +44,4 @@ combined[["SubjectNum"]] <- as.factor(combined[, SubjectNum])
 combined <- reshape2::melt(data = combined, id = c("SubjectNum", "Activity"))
 combined <- reshape2::dcast(data = combined, SubjectNum + Activity ~ variable, fun.aggregate = mean)
 
-data.table::fwrite(x = combined, file = "tidyData.txt", quote = FALSE)
+data.table::fwrite(x = combined, file = "tidyData.txt", quote = FALSE) 
